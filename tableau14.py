f2b233bd06c15ce09a20ecb20a579b39d456312f
AlexTheKing14 <alexandre17062612@gmail.com>
2025-03-17 13:43:27 +0100

    Update tableau14.py

--- tableau14.py
+++ tableau14.py
@@ -5,11 +5,16 @@ dico_hobbit["Age"] = 131
 print(dico_hobbit)
 
 print("____________")
+
 for cle in dico_hobbit:
     print(cle)
+
 print("____________")
+
 for val in dico_hobbit.values():
     print(val)
+
 print("____________")
+
 for (cle, val) in dico_hobbit.items():
     print(cle, "->", val)
