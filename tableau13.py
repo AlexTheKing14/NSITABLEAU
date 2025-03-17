16eb0d11d40297b9c98661ce3472950d5a4b762f
AlexTheKing14 <alexandre17062612@gmail.com>
2025-03-17 13:42:49 +0100

    tableau2

---
+++ tableau13.py
@@ -0,0 +1,5 @@
+dico_hobbit = {"prenom" : "Bilbon", "Age" : 111}
+print(dico_hobbit)
+print(len(dico_hobbit))
+print(dico_hobbit["prenom"])
+print(dico_hobbit["Age"])
---
+++ tableau14.py
@@ -0,0 +1,15 @@
+dico_hobbit = {"prenom" : "Bilbon", "Age" : 111}
+dico_hobbit ["nom"] = "Sacquet"
+
+dico_hobbit["Age"] = 131
+print(dico_hobbit)
+
+print("____________")
+for cle in dico_hobbit:
+    print(cle)
+print("____________")
+for val in dico_hobbit.values():
+    print(val)
+print("____________")
+for (cle, val) in dico_hobbit.items():
+    print(cle, "->", val)
